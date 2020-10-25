105e8b294ce6a817e481fa497fb13bdb54164918
Jakub Cabal <jakubcabal@gmail.com>
2020-10-25 16:04:06 +0100

    Add tester logic, fix some bugs

---
+++ rtl/comp/tester/tester.vhd
@@ -0,0 +1,176 @@
+--------------------------------------------------------------------------------
+-- PROJECT: SDRAM TESTER FPGA
+--------------------------------------------------------------------------------
+-- AUTHORS: Jakub Cabal <jakubcabal@gmail.com>
+-- LICENSE: The MIT License, please read LICENSE file
+--------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity SDRAM_TESTER is
+    Port (
+        -- CLOCK AND RESET
+        CLK      : in  std_logic;
+        RST      : in  std_logic;
+
+        -- WISHBONE SLAVE INTERFACE
+        WB_CYC     : in  std_logic;
+        WB_STB     : in  std_logic;
+        WB_WE      : in  std_logic;
+        WB_ADDR    : in  std_logic_vector(15 downto 0);
+        WB_DIN     : in  std_logic_vector(31 downto 0);
+        WB_STALL   : out std_logic;
+        WB_ACK     : out std_logic;
+        WB_DOUT    : out std_logic_vector(31 downto 0);
+
+        -- SDRAM TEST INTERFACE
+        TEST_ADDR  : out std_logic_vector(21 downto 0);
+        TEST_DWR   : out std_logic_vector(31 downto 0);
+        TEST_WR    : out std_logic;
+        TEST_VLD   : out std_logic;
+        TEST_RDY   : in  std_logic;
+        TEST_DRD   : in  std_logic_vector(31 downto 0);
+        TEST_DRD_V : in  std_logic
+    );
+end entity;
+
+architecture RTL of SDRAM_TESTER is
+
+    signal ctrl_reg_sel : std_logic;
+    signal ctrl_reg_we  : std_logic;
+    signal ctrl_reg     : std_logic_vector(31 downto 0);
+
+    signal len_reg_sel  : std_logic;
+    signal len_reg_we   : std_logic;
+    signal len_reg      : std_logic_vector(31 downto 0);
+
+    signal test_run     : std_logic;
+    signal test_clr     : std_logic;
+    signal test_mode    : std_logic;
+    signal test_ready   : std_logic;
+
+    signal addr_cnt     : unsigned(21 downto 0);
+    signal tick_cnt     : unsigned(31 downto 0);
+    signal tick_cnt_max : std_logic;
+    signal req_cnt      : unsigned(31 downto 0);
+    signal rdresp_cnt   : unsigned(31 downto 0);
+
+begin
+
+    ctrl_reg_sel <= '1' when (WB_ADDR = X"0000") else '0';
+    ctrl_reg_we  <= WB_STB and WB_WE and ctrl_reg_sel;
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (ctrl_reg_we = '1') then
+                ctrl_reg <= WB_DIN;
+            end if;
+        end if;
+    end process;
+
+    len_reg_sel <= '1' when (WB_ADDR = X"0008") else '0';
+    len_reg_we  <= WB_STB and WB_WE and len_reg_sel;
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                len_reg <= X"0FFFFFFF";
+            elsif (len_reg_we = '1') then
+                len_reg <= WB_DIN;
+            end if;
+        end if;
+    end process;
+
+    WB_STALL <= '0';
+
+    wb_ack_reg_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            WB_ACK <= WB_CYC and WB_STB;
+        end if;
+    end process;
+
+    wb_dout_reg_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            case WB_ADDR is
+                when X"0000" => -- control register
+                    WB_DOUT <= ctrl_reg;
+                when X"0004" => -- status register
+                    WB_DOUT    <= (others => '0');
+                    WB_DOUT(0) <= tick_cnt_max;
+                when X"0008" => -- length register
+                    WB_DOUT <= len_reg;
+                when X"0010" => -- tick counter
+                    WB_DOUT <= std_logic_vector(tick_cnt);
+                when X"0014" => -- request counter
+                    WB_DOUT <= std_logic_vector(req_cnt);
+                when X"0018" => -- read response counter
+                    WB_DOUT <= std_logic_vector(rdresp_cnt);
+                when others =>
+                    WB_DOUT <= X"DEADCAFE";
+            end case;
+        end if;
+    end process;
+
+    test_run   <= ctrl_reg(0) and not tick_cnt_max;
+    test_clr   <= ctrl_reg(1);
+    test_mode  <= ctrl_reg(4);
+    test_ready <= test_run and TEST_RDY;
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1' or test_clr = '1') then
+                addr_cnt <= (others => '0');
+            elsif (test_ready = '1') then
+                addr_cnt <= addr_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    TEST_ADDR <= std_logic_vector(addr_cnt);
+    TEST_DWR  <= std_logic_vector(addr_cnt(16-1 downto 0)) & std_logic_vector(addr_cnt(16-1 downto 0));
+    TEST_WR   <= test_mode;
+    TEST_VLD  <= test_run;
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1' or test_clr = '1') then
+                tick_cnt <= (others => '0');
+            elsif (test_run = '1' and tick_cnt_max = '0') then
+                tick_cnt <= tick_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    tick_cnt_max <= '1' when (tick_cnt = unsigned(len_reg)) else '0';
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1' or test_clr = '1') then
+                req_cnt <= (others => '0');
+            elsif (test_ready = '1') then
+                req_cnt <= req_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1' or test_clr = '1') then
+                rdresp_cnt <= (others => '0');
+            elsif (TEST_DRD_V = '1') then
+                rdresp_cnt <= rdresp_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+end architecture;
--- rtl/fpga.vhd
+++ rtl/fpga.vhd
@@ -20,11 +20,11 @@ entity FPGA is
         SDRAM_DQ    : inout std_logic_vector(16-1 downto 0);
         SDRAM_DQM   : out   std_logic_vector(2-1 downto 0);
         SDRAM_CLK   : out   std_logic;
-        SDRAM_CS    : out   std_logic;
+        SDRAM_CS_N  : out   std_logic;
         SDRAM_CKE   : out   std_logic;
-        SDRAM_RAS   : out   std_logic;
-        SDRAM_CAS   : out   std_logic;
-        SDRAM_WE    : out   std_logic;
+        SDRAM_RAS_N : out   std_logic;
+        SDRAM_CAS_N : out   std_logic;
+        SDRAM_WE_N  : out   std_logic;
         -- UART interface
         UART_RXD    : in    std_logic;
         UART_TXD    : out   std_logic
@@ -33,20 +33,16 @@ end entity;
 
 architecture FULL of FPGA is
 
-    constant WB_BASE_PORTS  : natural := 4;  -- system, eth, app (firewall), reserved
+    constant WB_BASE_PORTS  : natural := 4;  -- system, test, reserved, reserved
     constant WB_BASE_OFFSET : natural := 14;
-    constant WB_ETH_PORTS   : natural := 2;  -- eth0, eth1
-    constant WB_ETH_OFFSET  : natural := 13;
-    constant WB_APP_PORTS   : natural := 2;  -- app0, app1
-    constant WB_APP_OFFSET  : natural := 13;
 
-    signal rst_btn : std_logic;
+    signal rst_btn      : std_logic;
 
     signal pll_locked   : std_logic;
     signal pll_locked_n : std_logic;
 
-    signal clk_sdram : std_logic;
-    signal rst_sdram : std_logic;
+    signal clk_sdram    : std_logic;
+    signal rst_sdram    : std_logic;
 
     signal wb_master_cyc   : std_logic;
     signal wb_master_stb   : std_logic;
@@ -66,30 +62,16 @@ architecture FULL of FPGA is
     signal wb_mbs_ack   : std_logic_vector(WB_BASE_PORTS-1 downto 0);
     signal wb_mbs_dout  : std_logic_vector(WB_BASE_PORTS*32-1 downto 0);
 
-    signal wb_mes_cyc   : std_logic_vector(WB_ETH_PORTS-1 downto 0);
-    signal wb_mes_stb   : std_logic_vector(WB_ETH_PORTS-1 downto 0);
-    signal wb_mes_we    : std_logic_vector(WB_ETH_PORTS-1 downto 0);
-    signal wb_mes_addr  : std_logic_vector(WB_ETH_PORTS*16-1 downto 0);
-    signal wb_mes_din   : std_logic_vector(WB_ETH_PORTS*32-1 downto 0);
-    signal wb_mes_stall : std_logic_vector(WB_ETH_PORTS-1 downto 0);
-    signal wb_mes_ack   : std_logic_vector(WB_ETH_PORTS-1 downto 0);
-    signal wb_mes_dout  : std_logic_vector(WB_ETH_PORTS*32-1 downto 0);
-
-    signal wb_mas_cyc   : std_logic_vector(WB_APP_PORTS-1 downto 0);
-    signal wb_mas_stb   : std_logic_vector(WB_APP_PORTS-1 downto 0);
-    signal wb_mas_we    : std_logic_vector(WB_APP_PORTS-1 downto 0);
-    signal wb_mas_addr  : std_logic_vector(WB_APP_PORTS*16-1 downto 0);
-    signal wb_mas_din   : std_logic_vector(WB_APP_PORTS*32-1 downto 0);
-    signal wb_mas_stall : std_logic_vector(WB_APP_PORTS-1 downto 0);
-    signal wb_mas_ack   : std_logic_vector(WB_APP_PORTS-1 downto 0);
-    signal wb_mas_dout  : std_logic_vector(WB_APP_PORTS*32-1 downto 0);
+    signal test_addr    : std_logic_vector(21 downto 0);
+    signal test_dwr     : std_logic_vector(31 downto 0);
+    signal test_wr      : std_logic;
+    signal test_vld     : std_logic;
+    signal test_rdy     : std_logic;
+    signal test_drd     : std_logic_vector(31 downto 0);
+    signal test_drd_v   : std_logic;
 
     signal sdram_a_uns  : unsigned(12-1 downto 0);
     signal sdram_ba_uns : unsigned(2-1 downto 0);
-    signal sdram_cs_n   : std_logic;
-    signal sdram_ras_n  : std_logic;
-    signal sdram_cas_n  : std_logic;
-    signal sdram_we_n   : std_logic;
 
 begin
 
@@ -179,6 +161,31 @@ begin
         WB_DOUT  => wb_mbs_din((0+1)*32-1 downto 0*32)
     );
 
+    tester_i : entity work.SDRAM_TESTER
+    port map (
+        -- CLOCK AND RESET
+        CLK        => clk_sdram,
+        RST        => rst_sdram,
+
+        -- WISHBONE SLAVE INTERFACE
+        WB_CYC     => wb_mbs_cyc(1),
+        WB_STB     => wb_mbs_stb(1),
+        WB_WE      => wb_mbs_we(1),
+        WB_ADDR    => wb_mbs_addr((1+1)*16-1 downto 1*16),
+        WB_DIN     => wb_mbs_dout((1+1)*32-1 downto 1*32),
+        WB_STALL   => wb_mbs_stall(1),
+        WB_ACK     => wb_mbs_ack(1),
+        WB_DOUT    => wb_mbs_din((1+1)*32-1 downto 1*32),
+
+        TEST_ADDR  => test_addr,
+        TEST_DWR   => test_dwr,
+        TEST_WR    => test_wr,
+        TEST_VLD   => test_vld,
+        TEST_RDY   => test_rdy,
+        TEST_DRD   => test_drd,
+        TEST_DRD_V => test_drd_v
+    );
+
     sdram_ctrl_i : entity work.sdram
     generic map (
         CLK_FREQ         => 166.0,
@@ -191,7 +198,7 @@ begin
         SDRAM_BANK_WIDTH => 2,
         CAS_LATENCY      => 3, -- 2=below 133MHz, 3=above 133MHz
         BURST_LENGTH     => 2,
-        T_DESL           => 64000000.0, -- startup delay
+        T_DESL           => 200000.0, -- startup delay
         T_MRD            =>     12.0, -- mode register cycle time OK
         T_RC             =>     60.0, -- row cycle time OK
         T_RCD            =>     15.0, -- RAS to CAS delay OK
@@ -202,32 +209,28 @@ begin
     port map (
         reset       => rst_sdram,
         clk         => clk_sdram,
-        addr        => (others => '0'),
-        data        => (others => '1'),
-        we          => '1',
-        req         => '1',
-        ack         => open,
-        valid       => open,
-        q           => open,
+        addr        => unsigned(test_addr),
+        data        => test_dwr,
+        we          => test_wr,
+        req         => test_vld,
+        ack         => test_rdy,
+        valid       => test_drd_v,
+        q           => test_drd,
 
         sdram_a     => sdram_a_uns,
         sdram_ba    => sdram_ba_uns,
         sdram_dq    => SDRAM_DQ,
         sdram_cke   => SDRAM_CKE,
-        sdram_cs_n  => sdram_cs_n,
-        sdram_ras_n => sdram_ras_n,
-        sdram_cas_n => sdram_cas_n,
-        sdram_we_n  => sdram_we_n,
+        sdram_cs_n  => SDRAM_CS_N,
+        sdram_ras_n => SDRAM_RAS_N,
+        sdram_cas_n => SDRAM_CAS_N,
+        sdram_we_n  => SDRAM_WE_N,
         sdram_dqml  => SDRAM_DQM(0),
         sdram_dqmh  => SDRAM_DQM(1)
     );
 
     SDRAM_A   <= std_logic_vector(sdram_a_uns);
     SDRAM_BA  <= std_logic_vector(sdram_ba_uns);
-    SDRAM_CS  <= not sdram_cs_n;
-    SDRAM_RAS <= not sdram_ras_n;
-    SDRAM_CAS <= not sdram_cas_n;
-    SDRAM_WE  <= not sdram_we_n;
     SDRAM_CLK <= clk_sdram;
 
 end architecture;
